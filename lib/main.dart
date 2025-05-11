d46ec3a05fe5ab3e218a14bd5572b9f2c4e812ee
JoaoVarela1507 <joaovarela.cicm2@gmail.com>
2025-05-11 20:37:25 -0300

    Primeiro commit, adição do projeto

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:pphonedex/screens/register_screen.dart';
+import 'package:pphonedex/screens/splash_screen.dart';
+import 'package:pphonedex/components/custom_bottom_bar.dart';
+import 'package:pphonedex/screens/login_screen.dart';
+
+
+void main() {
+  runApp(const PhoneDexApp());
+}
+
+class PhoneDexApp extends StatelessWidget {
+  const PhoneDexApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PhoneDex',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        scaffoldBackgroundColor: const Color(0xFFD0D0D0),
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (_) => const SplashScreen(),
+        '/login': (_) => const LoginScreen(),
+        '/register': (_) => const RegisterScreen(),
+      },
+    );
+  }
+}
+
+
+
+
+
+
+
